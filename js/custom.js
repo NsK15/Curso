db0eeeba86c79e407787ccb29bd7b34335d34870
Neemias Simão <neemias3006@gmail.com>
2023-07-20 22:34:21 -0300

    jquery 2

--- js/custom.js
+++ js/custom.js
@@ -41,3 +41,64 @@ $('.alert').on('click', function (event) {
 
     alert('Produto Esgotado');
 })
+
+/* Callback
+* Entendendo ações que começam ao termino de outra
+*/
+$('.featured-item:nth(1)')
+    .hide(2000, function () {
+        // este é o callback
+        console.log($(this).find('h4').text() + ' esgotado')
+    })
+    .show(2000, function () {
+        console.log($(this).find('h4').text() + ' em esgotado')
+    })
+
+
+/*
+* Animações
+*/
+const duracao = 1000 // equivalente a 1 segundo
+
+$('.featured-item:nth(0)')
+    .hide(duracao)
+    .show(duracao)
+    .fadeOut(duracao)
+    .fadeIn(duracao)
+    .toggle(duracao)
+    .toggle(duracao)
+
+$('#form-submit').on('click', function (e) {
+
+    e.preventDefault()
+
+    if ($('#email').val() != '') {
+        $('#email').animate({
+            opacity: "toggle",
+            top: "-50"
+        }, duracao, function () {
+            console.log($(this).val())
+        })
+    }
+
+    /*
+    * Ouvinte de eventos .nav-modal-open
+    */
+    $('.nav-modal-open').on('click', function (e) {
+
+        e.preventDefault();
+
+        let elem = $(this).attr('rel')
+
+        $('.modal-body').html($('#' + elem).html())
+        $('.modal-header h5.modal-title').html($(this).text())
+
+        let myModal = new bootstrap.Modal($('#modalId'))
+
+        myModal.show()
+
+
+    })
+
+})
+
